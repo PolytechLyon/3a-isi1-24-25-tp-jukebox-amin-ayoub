94e2b66951813a8782005aaf1672008a48ab7d3f
Amin <darkfireyo@gmail.com>
2025-01-07 16:40:18 +0100

    initialize Vue 3 application with basic structure and styles

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!-- filepath: /w:/ISI1/3a-isi1-24-25-tp-jukebox-amin-ayoub/src/App.vue -->
+<template>
+  <div id="app">
+    <Controller />
+  </div>
+</template>
+
+<script>
+import Controller from './Controller.vue';
+
+export default {
+  components: {
+    Controller
+  }
+};
+</script>
+
+<style>
+/* Ajoutez vos styles ici */
+</style>
