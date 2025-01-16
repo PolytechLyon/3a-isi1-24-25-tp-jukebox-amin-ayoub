62cad15d323d7b6bc8e69edae72dcd6f07a1a537
Amin - Ayoub <ayoub.nadir@etu.univlyon1.fr>
2025-01-16 10:15:37 +0100

    style

--- src/components/PlayerController.vue
+++ src/components/PlayerController.vue
@@ -37,9 +37,14 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 .controls {
   display: flex;
   align-items: center;
+  margin-top: 10px;
 }
 </style>
